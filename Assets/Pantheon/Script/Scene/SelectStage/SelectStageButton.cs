191f5c3faa6cd05b0cdf562108f68f78da5075c4
TakedaShohei <takeda.syouhei@gmail.com>
2019-08-24 12:13:39 +0900

    ステージ選択ボタンスクリプト追加

---
+++ Assets/Pantheon/Script/Scene/SelectStage/SelectStageButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class SelectStageButton : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text_ = null;
+    StageModel model_ = null;
+
+
+    public void SetUp(StageModel model)
+    {
+        model_ = model;
+        text_.text = model_.Name;
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

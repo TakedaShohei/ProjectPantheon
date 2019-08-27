2585815f135825ad38a644b11ad49c4ec997e865
TakedaShohei <takeda.syouhei@gmail.com>
2019-08-27 23:48:24 +0900

    ボタンの文字を変更

--- Assets/Pantheon/Script/Scene/SelectStage/SelectStageScene.cs
+++ Assets/Pantheon/Script/Scene/SelectStage/SelectStageScene.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
-
+using UnityEngine.UI;
 
 public class SelectStageScene : MonoBehaviour
 {
-    [SerializeField]GameObject canvas_ = null; 
+    [SerializeField]GameObject canvas_=null; 
     // Use this for initialization
     void Start()
     {
@@ -14,9 +14,9 @@ public class SelectStageScene : MonoBehaviour
 
     void Init()
     {
-        GameObject button_prefab = Resources.Load("prehab/SelectStage/SelectStageButton") as GameObject;
+        GameObject button_prefab = Resources.Load("Prefab/SelectStage/SelectStageButton") as GameObject;
         StageInfo stage_info = Resources.Load("ScriptableObject/StageInfo") as StageInfo;
-        for(var i=0; i< stage_info.StageInfoList.Count; i++)
+        for(int i =0; i< stage_info.StageInfoList.Count; i++)
         {
             Debug.Log("test"+i);
             GameObject button = Instantiate(button_prefab, canvas_.transform);

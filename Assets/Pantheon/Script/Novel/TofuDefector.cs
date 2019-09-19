d903685f0b648aba4118deb03280bef8f9fe8fa8
TakedaShohei <takeda.syouhei@gmail.com>
2019-09-19 18:21:50 +0900

    サンプルのノベルデータをまねてある程度は完成。ただし、NovelSystemのチェックが外れる問題が残っている。

---
+++ Assets/Pantheon/Script/Novel/TofuDefector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+
+/// <summary>
+/// 豆腐を検出するコンポーネント
+/// </summary>
+[ExecuteInEditMode]
+public class TofuDetector : MonoBehaviour
+{
+    void Awake()
+    {
+        TMPro_EventManager.TEXT_CHANGED_EVENT.Add(ON_TEXT_CHANGED);
+    }
+
+    private void OnDestroy()
+    {
+        TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(ON_TEXT_CHANGED);
+    }
+
+    private void ON_TEXT_CHANGED(Object obj)
+    {
+        if (obj is TextMeshProUGUI)
+        {
+            var text = (TextMeshProUGUI)obj;
+
+            foreach (var chara in text.text)
+            {
+                TMP_Character glyph;
+                TMP_FontUtilities.SearchForCharacter(text.font, chara, out glyph);
+
+                if (glyph == null)
+                {
+                    Debug.Log("Detection Tofu!! " + chara);
+                    // ここでtxt等にログを残したり、slackAPIを叩いたり等
+                }
+            }
+        }
+    }
+}
